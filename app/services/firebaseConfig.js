ed556deb45bca1458e01325ea0f0c8ada2892429
Aadhiya <aadhiyamaria@gmail.com>
2024-12-11 10:45:14 +0300

    Update firebaseConfig.js

--- app/services/firebaseConfig.js
+++ app/services/firebaseConfig.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyArCs5z5h5kHZ6KF1QNkwMYaDRBxxkEj8k",
+  apiKey: "",
   authDomain: "renteasy-a2dea.firebaseapp.com",
   projectId: "renteasy-a2dea",
   storageBucket: "renteasy-a2dea.appspot.com",
   messagingSenderId: "548704032594",
-  appId: "1:548704032594:web:56a456c69402fe77a94c8b"
+  appId: ""
 };
 
 // Initialize Firebase
